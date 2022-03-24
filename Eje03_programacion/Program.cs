f8ff7efd4859d57b3cbf7e7e031615b24ebf34ec
ssalazar446 <ssalazar446@misena.edu.co>
2022-03-24 17:51:42 -0500

    Ejercicios del 17 al 28

--- Eje03_programacion/Program.cs
+++ Eje03_programacion/Program.cs
@@ -165,6 +165,226 @@ namespace Eje03_programacion
             {
 				Console.WriteLine("Los numeros no son iguales");
             }
+
+			//18. Pedir un número e indicar si es positivo o negativo.
+			double numero1;
+			Console.WriteLine("Digite un numero");
+			numero1 = double.Parse(Console.ReadLine());
+
+			if (numero1 > 0)
+            {
+				Console.WriteLine("El numero es positivo");
+            } else
+            {
+				Console.WriteLine("El numero es negativo");
+            }
+
+			//19. Pedir dos números y decir si uno es múltiplo del otro.
+			double number1, number2;
+			Console.WriteLine("Digite numero 1");
+			number1 = double.Parse(Console.ReadLine());
+			Console.WriteLine("Digite numero 2");
+			number2 = double.Parse(Console.ReadLine());
+
+			if(a % b == 0)
+            {
+				Console.WriteLine("Es multiplo");
+            } else
+            {
+				Console.WriteLine("No es multiplo");
+            }
+
+			//20. Pedir dos números y decir cuál es el mayor.
+			double number_1, number_2;
+			Console.WriteLine("Digite numero 1");
+			number1 = double.Parse(Console.ReadLine());
+			Console.WriteLine("Digite numero 2");
+			number2 = double.Parse(Console.ReadLine());
+			
+			if (number_1 > number_2)
+            {
+				Console.WriteLine("El primer numero es mayor que el segundo");
+            } else
+            {
+				Console.WriteLine("El segundo numero es mayor que el primero");
+            }
+
+			//21. Pedir dos números y decir cuál es el mayor o si son iguales.
+			double one, two;
+			Console.WriteLine("Digite numero 1");
+			one = double.Parse(Console.ReadLine());
+			Console.WriteLine("Digite numero 2");
+			two = double.Parse(Console.ReadLine());
+
+			if(one == two)
+            {
+				Console.WriteLine("Los numeros son iguales");
+            }
+			if(one > two)
+            {
+				Console.WriteLine("El primer numero es mayor que el segundo");
+            } else
+            {
+				Console.WriteLine("El segundo numero es mayor que el primero");
+            }
+
+			//22. Pedir dos números y mostrarlos ordenados de mayor a menor.
+			double numero_uno, numero_dos;
+			Console.WriteLine("Digite numero 1");
+			numero_uno = double.Parse(Console.ReadLine());
+			Console.WriteLine("Digite numero 2");
+			numero_dos = double.Parse(Console.ReadLine());
+
+			if(numero_uno > numero_dos)
+            {
+				Console.WriteLine("El orden de los numeros es: {0} y {1}", numero_uno, numero_dos);
+            } else
+            {
+				Console.WriteLine("El orden de los numeros es: {0} y {1}", numero_dos, numero_uno);
+            }
+
+			//23. Pedir tres números y mostrarlos ordenados de mayor a menor.
+			double uno_1, dos_2, tres_3;
+			Console.WriteLine("Digite numero 1");
+			uno_1 = double.Parse(Console.ReadLine());
+			Console.WriteLine("Digite numero 2");
+			dos_2 = double.Parse(Console.ReadLine());
+			Console.WriteLine("Digite numero 2");
+			tres_3 = double.Parse(Console.ReadLine());
+
+			if(uno_1 > dos_2 && dos_2 > tres_3) { 
+				Console.WriteLine("El orden de los numeros es: {0} y {1} y {2}", uno_1, dos_2, tres_3);
+				}
+				if (dos_2 > uno_1 && tres_3 > uno_1) { 
+				Console.WriteLine("El orden de los numeros es: {0} y {1} y {2}", dos_2, tres_3, uno_1);
+					}
+				if (tres_3 > dos_2 && uno_1 > dos_2) { 
+				Console.WriteLine("El orden de los numeros es: {0} y {1} y {2}", tres_3, uno_1, dos_2);
+					}
+
+
+			//24. Pedir un número entre 0 y 9.999 y decir cuántas cifras tiene.
+			double cifras;
+			Console.WriteLine("Digite numero 1");
+			cifras = double.Parse(Console.ReadLine());
+            if(cifras < 100)
+            {
+				Console.WriteLine("El numero tiene 2 cifras");
+            } if(cifras < 1000)
+            {
+				Console.WriteLine("El numero tiene 3 cifras");
+            } if(cifras < 10000)
+            {
+				Console.WriteLine("El numero tiene 4 cifras");
+            }
+
+			//25. Pedir una nota de 0 a 5 y mostrarla de la forma: Insuficiente (0 ? 2,9), Suficiente (3 ? 4,5) y Bien (4,6? 5)
+			double nota;
+			Console.WriteLine("Digite numero 1");
+			nota = double.Parse(Console.ReadLine());
+
+			if(nota < 2.9)
+            {
+				Console.WriteLine("Insuficiente");
+            } if(nota > 3 && nota < 4.5) 
+            {
+				Console.WriteLine("Suficiente");
+            } if(nota > 4.6 && nota < 5)
+            {
+				Console.WriteLine("Bien");
+            }
+			else
+            {
+				Console.WriteLine("Numero fuera de rango");
+            }
+
+			//26. Pedir una nota numérica entera entre 0 y 10, y mostrar dicha nota de la forma: cero, uno, dos, tres...
+			double leer_numero;
+			Console.WriteLine("Digite numero 1");
+			leer_numero = double.Parse(Console.ReadLine());
+			if (leer_numero == 0)
+            {
+				Console.WriteLine("Cero");
+            }
+			if (leer_numero == 1)
+            {
+				Console.WriteLine("Uno");
+            }
+			if (leer_numero == 3)
+            {
+				Console.WriteLine("Tres");
+            }
+			if (leer_numero == 4)
+            {
+				Console.WriteLine("Cuatro");
+            }
+			if (leer_numero == 5)
+            {
+				Console.WriteLine("Cinco");
+            }
+			if (leer_numero == 6)
+            {
+				Console.WriteLine("Seis");
+            }
+			if (leer_numero == 7)
+            {
+				Console.WriteLine("Siete");
+            }
+			if (leer_numero == 8)
+            {
+				Console.WriteLine("Ocho");
+            }
+			if (leer_numero == 9)
+            {
+				Console.WriteLine("Nueve");
+            }
+			if (leer_numero == 10)
+            {
+				Console.WriteLine("Diez");
+            }
+
+
+			//27. Pedir un número y decir si es par o impar.
+			int numero_par;
+			Console.WriteLine("Digite un numero");
+			numero_par = int.Parse(Console.ReadLine());
+
+			if ((numero_par % 2) == 0)
+            {
+				Console.WriteLine("Es par");
+            } else
+            {
+				Console.WriteLine("Es impar");
+            }
+			//28. Un trabajador recibe su pago, según la cantidad de horas trabajadas y su valor. Si la cantidad de
+			//horas trabajadas es mayor que 40, éstas se consideran horas extra, y tienen un incremento de $10000
+			//(diez mil) sobre el valor de la hora. Calcular y mostrar el salario (pago) del trabajador. Nota: leer horas
+			//trabajadas y valor de la hora.
+			int cantidadHoras, valor_horas, horas_extra;
+			double pago_total, pago_extra;
+		
+
+			Console.WriteLine("Ingrese el valor de la hora");
+			valor_horas = int.Parse(Console.ReadLine());
+			Console.WriteLine("Ingrese la cantidad de la horas trabajadas");
+			cantidadHoras = int.Parse(Console.ReadLine());
+			pago_total = valor_horas * cantidadHoras;
+
+			if(cantidadHoras < 40)
+            {
+				
+				Console.WriteLine("Su pago es: ",  pago_total);
+				
+            }
+			if (cantidadHoras > 40)
+            {
+				horas_extra = valor_horas ++ 10000;
+				pago_extra = valor_horas * cantidadHoras;
+				Console.WriteLine("Su pago es: ",  pago_extra);
+				
+            }
+
+
 		}
 	}
 }
